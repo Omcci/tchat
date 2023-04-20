dba2553b743ab11de49347aab2126808f7882d83
Alexandre Renard <16491342+Alexandre78R@users.noreply.github.com>
2023-04-20 11:31:03 +0200

    Sauvegarde et Avancement des test du multi joueur

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 import env from './env.js';
 
 var ipBackend = `http://${env.ip}:${env.portBackend}/`;
+const socketOpen = io(ipBackend);
 
 function App() {
   const [socket, setSocket] = useState(null);
@@ -24,30 +25,91 @@ function App() {
   // }
 
   const handleJoinRoom = (username, room) => {
-    socket.emit('joinRoom', username, room);
-    socket.emit('roomJoined', console.log());
+    // socketOpen.on('joinRoom', username, room);
+    socketOpen.emit('joinRoom', username, room);
+    socketOpen.emit('roomJoined', console.log());
   }
 
 
-  function handleRoomChange(event) {
+  const  handleRoomChange = (event) => {
     setRoom(event.target.value);
   }
 
-  function handlePseudoChange(event) {
+  const handlePseudoChange = (event) => {
     setPseudo(event.target.value);
   }
 
 
-  function handleConnect() {
+  const handleConnect = () => {
     const newSocket = io(ipBackend, { transports: ["polling"] });
     setSocket(newSocket);
     console.log("socket", socket)
   }
 
-  function handleListPlayer () {
-    console.log(socket.emit('getPlayers', setPlayers()))
-    socket.emit('getPlayers', console.log());
+   const handleClick = () => {
+    console.log("test");
+  //   // console.log(socket.emit('getPlayers', setPlayers()))
+  //   socket.emit('getPlayers', (data) => {
+  //     console.log("data --->", data)
+  //     setPlayers(data)
+  //   });
+  //   // socketOpen.on('getPlayers', (data) => {
+  //   //   console.log("data --->", data)
+  //   //   setPlayers(data)
+  //   // });
   }
+
+  // useEffect(() => {
+  //   // // Requête pour récupérer les joueurs de la room
+  //   // socketOpen.on('getPlayers', (data) => {
+  //   //   console.log('data Open composent', data); 
+  //   //   setPlayers(data);
+  //   // });
+
+  //   // // Écouteur pour mettre à jour la liste des joueurs lorsqu'elle change
+  //   // socketOpen.on('playersUpdate', (data) => {
+  //   //   setPlayers(data);
+  //   // });
+
+  //   // // Nettoyage du composant lorsqu'il est démonté
+  //   // return () => {
+  //   //   socketOpen.off('playersUpdate');
+  //   // }
+
+  //   socket.on('getPlayers', (players) => {
+  //     setPlayers(players);
+  //   });
+
+  //   // getPlayers();
+
+  //   // return () => {
+  //   //   socket.off('playersUpdate');
+  //   // };
+  // }, []);
+
+  useEffect(() => {
+    // Écouteur pour mettre à jour la liste des joueurs lorsqu'elle change
+    socketOpen.on('playersUpdate', (data) => {
+      setPlayers(data);
+    });
+  
+    // Nettoyage du composant lorsqu'il est démonté
+    return () => {
+      socketOpen.off('playersUpdate');
+    };
+  }, []);
+  
+  // Envoie une demande pour récupérer les joueurs
+  useEffect(() => {
+    // socket.emit('getPlayers', (data) => {
+    //   console.log('data received from server', data);
+    //   setPlayers(data);
+    // });
+    socketOpen.on('getPlayers', (data) => {
+      console.log("data", data);
+      setPlayers(data);
+    });
+  }, [socket]);
 
   return (
     <div className="App">
@@ -66,16 +128,20 @@ function App() {
             <button onClick={() => handleJoinRoom(pseudo , room)}>Rejoindre une salle</button>
           </div>
           {/* {console.log(socket)} */}
-          <button onClick={() => handleListPlayer()}>liste joueur</button>
-          {players ?
+          <button onClick={handleClick}>liste joueur</button>
+          {console.log("backend ---> frontend ", players)}
+          {/* {players} */}
+          {players && players.length > 0 ?
             <div>
               {console.log("backend ---> frontend ", players)}
-              <p>        
-                - {players}
-              </p>
+              <ul>
+                {players.map((player) => (
+                  <li key={player.id}>{player.username}</li>
+                ))}
+              </ul>
             </div>
           :
-          ""
+          "No players"
           }
         </>
       )}
